83fa983300cb4ea8068ccb7d2a9c3465bcef4eee
JoanVelasco <jvelasco.estanyol@gmail.com>
2018-07-12 17:57:21 +0200

    Basic buttons added

--- tkinter-GUI.py
+++ tkinter-GUI.py
@@ -1,5 +1,20 @@
 import tkinter as tk
 
-top = tk.Tk()
+def connect():
+	print("connected")
+
+def disconnect():
+	print("disconnected")
+
+
+root = tk.Tk()
+root.title("Synchronization GUI")
+frame = tk.Frame(root).pack()
+
 #Add things to GUI some more
-top.mainloop()
+w = tk.Label(root, text="Synchronization Data").pack()
+exitBut = tk.Button(frame, text = "Exit", command = quit).pack(side = tk.RIGHT)
+connectBut = tk.Button(frame, text = "Connect", command = connect).pack(side = tk.LEFT)
+DisconnectBut = tk.Button(frame, text = "Disconnect", command = disconnect).pack(side = tk.LEFT)
+
+root.mainloop()
