20c3d8062ba26206c07dcbe4200422b0972cfccf
JoanVelasco <jvelasco.estanyol@gmail.com>
2018-07-13 20:13:32 +0200

    Text Area added

--- tkinter-GUI.py
+++ tkinter-GUI.py
@@ -7,10 +7,15 @@ def connect():
 def disconnect():
 	print("disconnected")
 
+def clearTextArea():
+	print("Text are cleared")
+
+
 
 root = Tk()
 root.title("Synchronization GUI")
 
+#Frame tree to devide the space
 windowFrame = Frame(root)
 windowFrame.pack(fill = BOTH, expand = 1, padx = 40, pady = 20)
 
@@ -20,15 +25,27 @@ titleFrame.pack(fill = X, pady = 5)
 mainFrame = Frame(windowFrame, bg = "red")
 mainFrame.pack(fill = BOTH, expand = 1, pady = 5)
 
+optButFrame = Frame(mainFrame, bg = "blue")
+optButFrame.pack(fill = Y, side = RIGHT, padx = 5)
+
 buttonsFrame = Frame(windowFrame, bg = "green")
 buttonsFrame.pack(fill = X, pady = 5)
 
 #Add things to GUI some more
-w = Label(titleFrame, text="Synchronization Data", font = "Segoe 16 bold").pack(side = LEFT)
+#Title and buttons
+title = Label(titleFrame, text="Synchronization Data", font = "Segoe 16 bold").pack(side = LEFT)
 exitBut = Button(buttonsFrame, text = "Exit", command = root.destroy).pack(side = RIGHT, ipadx = 20)
 connectBut = Button(buttonsFrame, text = "Connect", command = connect).pack(side = LEFT, ipadx = 20)
-DisconnectBut = Button(buttonsFrame, text = "Disconnect", command = disconnect).pack(side = LEFT, ipadx = 20, padx = 30)
+disconnectBut = Button(buttonsFrame, text = "Disconnect", command = disconnect).pack(side = LEFT, ipadx = 20, padx = 30)
+clearBut = Button(optButFrame, text = "Clear", command = clearTextArea).pack(side = RIGHT, ipadx = 20)
 
+#Text area and scrollbar
+Scroll = Scrollbar(mainFrame)
+TextArea = Text(mainFrame, height=4) #heigth not releveant, for in pack it is said to occupy all the space, however if its not set the buttons below experience some problems
+Scroll.pack(side=RIGHT, fill=Y)
+TextArea.pack(side=LEFT, fill=BOTH)
+Scroll.config(command=TextArea.yview)
+TextArea.config(yscrollcommand=Scroll.set)
 
 
 root.geometry("800x500+20+25")
