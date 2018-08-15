f9f28de5b10411bf061d4f3ff40e5b4d0f0d53e3
JoanVelasco <jvelasco.estanyol@gmail.com>
2018-08-15 11:35:49 +0200

    Final version fully functional

--- tkinter-GUI.py
+++ tkinter-GUI.py
@@ -1,14 +1,16 @@
+from __future__ import division
+import struct
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-#import matplotlib.animation as animation
 from matplotlib import style
 from tkinter import *
 from tkinter import font
 import serial
 import threading
 import time
+
 
 OUT_STATE = 0
 SCAN_STATE = 1
@@ -17,29 +19,44 @@ CONNECTING_STATE = 3
 style.use("ggplot")
 
 handleMutex = threading.Semaphore(0)
+FileList = ('S1data.txt', 'S2data.txt', 'S3data.txt', 'S4data.txt')
 
 conn = None
 readThread = None
 state = OUT_STATE
 sensortagList = []
 macList = []
-emptyByte = b''
 
-figure = Figure(figsize=(5,5), dpi=100)
+LineSeparator = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Information = "This file contains acceleration values of x,y and z axis!"
+DateTimeNow = "Measurements taken by Joan Velasco on: " + time.strftime("%c") #%c = prints appropiate date and time representation
+headerFile = LineSeparator + "\n" + Information  + "\n"+ DateTimeNow + "\n" + LineSeparator + "\n" + "Accel-x [G], Accel-y [G], Accel-z [G]" + "\n"
+
+# w+ means --> open a file for writing and create it if it does not exist
+for filename in FileList:
+	with open(filename, 'w+') as file:
+		file.write(headerFile)
+		file.close()
+
+figure = Figure(figsize=(5,6), dpi=70)
 graphic1 = figure.add_subplot(311)
-graphic1.plot([1,2,3,4,5,6,7,8], [1,6,1,3,8,9,3,5])
-
 graphic2 = figure.add_subplot(312)
-graphic2.plot([1,2,3,4,5,6,7,8], [2,6,1,3,8,9,3,5])
-
 graphic3 = figure.add_subplot(313)
-graphic3.plot([1,2,3,4,5,6,7,8], [3,6,1,3,8,9,3,5])
+graphic1.set_ylabel('Acc X (G)')
+graphic2.set_ylabel('Acc Y (G)')
+graphic3.set_ylabel('Acc Z (G)')
+graphic3.set_xlabel('Time (s)')
+
 
 class sensorTag(object):
 	def __init__(self, name, mac):
 		self.name = name
 		self.mac = mac
 		self.handle = None
+		self.xAcc = []
+		self.yAcc = []
+		self.zAcc = []
+		self.time = []
 
 class connection(object):
 	def __init__(self, connected = False, port = 'COM12', baud = 115200, serPort = None):
@@ -47,6 +64,17 @@ class connection(object):
 		self.port = port #define the port
 		self.baud = baud #define bit rate
 		self.serPort = serPort
+
+def updateGraphs():
+	graphic1.clear()
+	graphic2.clear()
+	graphic3.clear()
+	for sensor in sensortagList:
+		graphic1.plot(sensor.time, sensor.xAcc, '.-', label = sensor.name)
+		graphic2.plot(sensor.time, sensor.yAcc, '.-', label = sensor.name)
+		graphic3.plot(sensor.time, sensor.zAcc, '.-', label = sensor.name)
+		graphic1.legend(loc = 'upper left', ncol = 4)
+	canvas.draw()
 
 def connect():
 	global conn
@@ -67,7 +95,6 @@ def disconnect():
 	conn.connected = False
 	readThread.join()
 	conn = None
-	#StkConn.join()
 	print("disconnected")
 	TextArea.insert(END, "Disconnected\n")
 
@@ -115,13 +142,8 @@ def connectStk():
 			#connect to sensor tag
 			conn.serPort.write(b'\x01\x09\xFE\x09\x00\x00\x00' + sensor.mac)
 			handleMutex.acquire()
+
 			#Activate notifications and configure sensors
-			conn.serPort.write(b'\x01\x8A\xFD\x04' + sensor.handle + b'\x11\x00')
-			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x8A\xFD\x04' + sensor.handle + b'\x1E\x00')
-			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x1F\x00\x01\x00')
-			time.sleep(0.5)
 			conn.serPort.write(b'\x01\x92\xFD\x05' + sensor.handle + b'\x53\x00\x02')
 			time.sleep(0.5)
 			conn.serPort.write(b'\x01\x8A\xFD\x04' + sensor.handle + b'\x51\x00')
@@ -130,32 +152,16 @@ def connectStk():
 			time.sleep(0.5)
 			conn.serPort.write(b'\x01\x92\xFD\x05' + sensor.handle + b'\x53\x00\x01')
 			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x2d\x00\x01\x00')
-			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x35\x00\x01\x00')
-			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x45\x00\x01\x00')
-			time.sleep(0.5)
 			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x3d\x00\x01\x00')
 			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x4d\x00\x01\x00')
-			time.sleep(0.5)
+			#conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x44\x00\x01\x00')
+			#time.sleep(0.5)
+
 		#Enable sensors
 		for sensor in sensortagList:
-			#conn.serPort.write(b'\x01\x92\xFD\x05' + sensor.handle + b'\x47\x00\x01')
-			#time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x3f\x00\x38\x02')
-			time.sleep(0.5)
-			conn.serPort.write(b'\x01\x8A\xFD\x04' + sensor.handle + b'\x3f\x00')
-			time.sleep(0.5)
-			#conn.serPort.write(b'\x01\x92\xFD\x05' + sensor.handle + b'\x2f\x00\x01')
-			#time.sleep(0.5)
 			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x3f\x00\x3f\x02')
 			time.sleep(0.5)
-			#conn.serPort.write(b'\x01\x92\xFD\x05' + sensor.handle + b'\x37\x00\x01')
-			#time.sleep(0.5)
-			conn.serPort.write(b'\x01\x92\xFD\x06' + sensor.handle + b'\x3f\x00\x7f\x02')
-			time.sleep(0.5)
+
 		state = DATA_STATE
 			
 
@@ -167,37 +173,29 @@ def connectStk():
 def InitLaunchpad():
 	conn.serPort.write(b'\x01\x03\x0C\x00')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(conn.serPort.in_waiting), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(conn.serPort.in_waiting))
 	conn.serPort.write(b'\x01\x00\xfe\x26\x08\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(9), emptyByte)
+	handleData(conn.serPort.read(9))
 	while conn.serPort.in_waiting <= 0: pass
-	#time.sleep(0.2)
-	handleData(conn.serPort.read(47), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(47))
 	conn.serPort.write(b'\x01\x31\xFE\x01\x15')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(11), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(11))
 	conn.serPort.write(b'\x01\x31\xFE\x01\x16')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(11), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(11))
 	conn.serPort.write(b'\x01\x31\xFE\x01\x1A')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(11), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(11))
 	conn.serPort.write(b'\x01\x31\xFE\x01\x19')
 	while conn.serPort.in_waiting <= 0: pass
-	handleData(conn.serPort.read(11), emptyByte)
-	#time.sleep(0.2)
+	handleData(conn.serPort.read(11))
 
 #Thread functions
-def handleData(header, data):
+def handleData(data):
 	print("handling data")
-	#TextArea.insert(END, data.decode()+"\n") # "utf-8" parameter of decode
-	TextArea.insert(END,(''.join(" " + format(x, '02x') for x in header) + ''.join(" " + format(x, '02x') for x in data) + "\n"))
+	TextArea.insert(END, ''.join(" " + format(x, '02x') for x in data) + "\n")
 	TextArea.see(END)
 
 
@@ -210,8 +208,8 @@ def readPort():
 		while conn.serPort.in_waiting > 0:
 			header = conn.serPort.read(3)
 			data = conn.serPort.read(header[2])
-			print(type(data))
-			handleData(header, data)
+			if state != DATA_STATE:
+				handleData(header + data)
 			if(state == OUT_STATE):
 				#Default
 				if (data[0] == 0x7F and data[1] == 0x06): #Data[0] is actually the 4th byte of the transmission, the first 3 are in "header"
@@ -226,16 +224,14 @@ def readPort():
 					TextArea.insert(END,("Scanning process finished\n"))
 					state = OUT_STATE
 				elif 0x54 in data:
-					#Find mac and name
-					#TextArea.insert(END, data.decode()+"\n")
+					#Find mac and create sensortag object
 					i = data.find(0x54)
 					mac = data[i-5:i+1]
 					if mac not in macList:
 						macList.append(mac)
 						sensor = sensorTag(name = "SensorTag"+str(len(sensortagList)), mac = mac)
 						sensortagList.append(sensor)
-
-					handleData(mac, emptyByte)
+					handleData(mac)
 
 			elif state == CONNECTING_STATE:
 				if 0x54 in data:
@@ -252,12 +248,34 @@ def readPort():
 				
 			elif(state == DATA_STATE):
 				#Messages from the sensortags
-				pass
-			else:
-				#Default2?? do I need it??
-				pass
-	conn.serPort.__del__() #conn.serPort.close()
+				if (data[0] == 0x1B and data[1] == 0x05 and data[5] == 0x14 and data[6] == 0x3C):
+					accRaw = struct.unpack('<hhh', data[14:20])
+					acc = [x / 4096.0 for x in accRaw]
+					sensortagList[int(data[3])].xAcc.append(acc[0])
+					sensortagList[int(data[3])].yAcc.append(acc[1])
+					sensortagList[int(data[3])].zAcc.append(acc[2])
 
+					time = struct.unpack('<HH', data[22:])
+					timeStructured = time[1] + (time[0] / 65536)
+					sensortagList[int(data[3])].time.append(timeStructured)
+
+					TextArea.insert(END, '%.3f' % (timeStructured) + " --> " + sensortagList[int(data[3])].name + ":  " + "X: %.5f, Y: %.5f, Z: %.5f" % (acc[0], acc[1], acc[2]) + "\n")
+					TextArea.see(END)
+
+					with open(FileList[int(data[3])], 'a') as file:	#'a' is to add data to the file.
+						file.write('%.3f' % (timeStructured) + " --> " + sensortagList[int(data[3])].name + ":  " + "X: %.5f, Y: %.5f, Z: %.5f" % (acc[0], acc[1], acc[2]) + "\n")
+						file.close()
+
+					if len(sensortagList[int(data[3])].xAcc) > 50:
+						#Pop first element of all the arrays[handle]
+						sensortagList[int(data[3])].xAcc.pop(0)
+						sensortagList[int(data[3])].yAcc.pop(0)
+						sensortagList[int(data[3])].zAcc.pop(0)
+						sensortagList[int(data[3])].time.pop(0)
+
+					updateGraphs()
+
+	conn.serPort.__del__()
 
 
 
@@ -268,19 +286,34 @@ root.title("Synchronization GUI")
 windowFrame = Frame(root)
 windowFrame.pack(fill = BOTH, expand = 1, padx = 40, pady = 20)
 
-titleFrame = Frame(windowFrame, bg = "green")
+titleFrame = Frame(windowFrame)
 titleFrame.pack(fill = X, pady = 5)
 
-mainFrame = Frame(windowFrame, bg = "red")
+
+
+mainFrame = Frame(windowFrame)
 mainFrame.pack(fill = BOTH, expand = 1, pady = 5)
 
-graphicsFrame = Frame(mainFrame, bg = "purple")
-graphicsFrame.pack(fill = BOTH, expand = 1, side = LEFT)
+optButFrame = Frame(mainFrame)
+optButFrame.pack(fill = Y, side = RIGHT)
 
-optButFrame = Frame(mainFrame, bg = "blue")
-optButFrame.pack(fill = Y, side = RIGHT, padx = 5)
 
-buttonsFrame = Frame(windowFrame, bg = "green")
+dataFrame = Frame(mainFrame)
+dataFrame.pack(fill = BOTH, expand = 1, side = LEFT)
+
+graphicsFrame = Frame(dataFrame)
+graphicsFrame.grid(row=0, column=0, sticky="nsew")
+
+textFrame = Frame(dataFrame)
+textFrame.grid(row=0, column=1, sticky="nsew")
+
+dataFrame.grid_columnconfigure(0, weight=1, uniform="group1")
+dataFrame.grid_columnconfigure(1, weight=1, uniform="group1")
+dataFrame.grid_rowconfigure(0, weight=1)
+
+
+
+buttonsFrame = Frame(windowFrame)
 buttonsFrame.pack(fill = X, pady = 5)
 
 #Add things to GUI some more
@@ -289,15 +322,16 @@ title = Label(titleFrame, text="Synchronization Data", font = "Segoe 16 bold").p
 exitBut = Button(buttonsFrame, text = "Exit", command = root.destroy).pack(side = RIGHT, ipadx = 20)
 connectBut = Button(buttonsFrame, text = "Connect", command = connect).pack(side = LEFT, ipadx = 20)
 disconnectBut = Button(buttonsFrame, text = "Disconnect", command = disconnect).pack(side = LEFT, ipadx = 20, padx = 30)
-scanBut = Button(optButFrame, text = "Scan", command = scan).pack(ipadx = 20)
+subtitle = Label(optButFrame, text="Sensortag Buttons:", font = "Segoe 10 bold").pack(side = TOP)
+scanBut = Button(optButFrame, text = "Scan", command = scan).pack(ipadx = 20, pady = 30)
 connectStkBut = Button(optButFrame, text = "Connect", command = startConnectStkThread).pack(ipadx = 20)
-disconnectStkBut = Button(optButFrame, text = "Disonnect", command = disconnectStk).pack(ipadx = 20)
+disconnectStkBut = Button(optButFrame, text = "Disonnect", command = disconnectStk).pack(ipadx = 20, pady = 10)
 clearBut = Button(optButFrame, text = "Clear", command = clearTextArea).pack(side = BOTTOM, ipadx = 20)
 
 
 #Text area and scrollbar
-Scroll = Scrollbar(mainFrame)
-TextArea = Text(mainFrame, height = 4) #heigth not releveant, for in pack it is said to occupy all the space, however if its not set the buttons below experience some problems
+Scroll = Scrollbar(textFrame)
+TextArea = Text(textFrame, height = 4) #heigth not releveant, for in pack it is said to occupy all the space, however if its not set the buttons below experience some problems
 Scroll.pack(side=RIGHT, fill=Y)
 TextArea.pack(side=LEFT, fill=BOTH, expand = 1)
 Scroll.config(command=TextArea.yview)
@@ -311,5 +345,5 @@ canvas.get_tk_widget().pack(side=TOP, fill= BOTH, expand = 1)
 
 
 
-root.geometry("800x600+20+25")
+root.geometry("900x600+20+25")
 root.mainloop()
